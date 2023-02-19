c12babe4ef566d95f1436a435ba99b7add802d15
Thomas <71586988+Tommypop2@users.noreply.github.com>
2023-02-19 17:52:52 +0000

    Updated std_lib

--- src/defaultFiles/std_lib.js
+++ src/defaultFiles/std_lib.js
@@ -1,6 +1,21 @@
-async function print(data) {
-  $global.log(data);
-}
-async function input() {
-  return $global.input();
+$std: {
+  async function print(data) {
+    $global.log(data);
+  }
+  async function input() {
+    return $global.input();
+  }
+  // Casts
+  async function str(data) {
+    return data.toString();
+  }
+  async function int(data) {
+    return parseInt(data);
+  }
+  async function float(data) {
+    return parseFloat(data);
+  }
+  async function bool(data) {
+    return data == true;
+  }
 }

3e12bb7d15ffed0f7e2f633951fbe1f669858ceb
Thomas <71586988+Tommypop2@users.noreply.github.com>
2023-02-19 22:25:36 +0000

    Added rationale as to why the compiler prepends all function calls with await

--- src/defaultFiles/std_lib.js
+++ src/defaultFiles/std_lib.js
@@ -1,3 +1,7 @@
+// Currently compiler dumps "await" everywhere. This is necessary for inputs to work, as the main thread on
+// web workers cannot be blocked whilst still having them receive messages. Therefore, the web worker must await a promise.
+// Eventually, the 'Result' window will go inside of an iframe, which should remove this problem, as print and input methods
+// will be able to directly manipulate the DOM
 async function print(data) {
   $global.log(data);
 }

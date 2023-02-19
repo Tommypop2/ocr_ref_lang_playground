953611d05570e643e16b4ba6bb4f9e8b5c9904e0
Thomas <71586988+Tommypop2@users.noreply.github.com>
2023-02-19 17:56:58 +0000

    Removed label

--- src/defaultFiles/std_lib.js
+++ src/defaultFiles/std_lib.js
@@ -1,21 +1,19 @@
-$std: {
-  async function print(data) {
-    $global.log(data);
-  }
-  async function input() {
-    return $global.input();
-  }
-  // Casts
-  async function str(data) {
-    return data.toString();
-  }
-  async function int(data) {
-    return parseInt(data);
-  }
-  async function float(data) {
-    return parseFloat(data);
-  }
-  async function bool(data) {
-    return data == true;
-  }
+async function print(data) {
+  $global.log(data);
+}
+async function input() {
+  return $global.input();
+}
+// Casts
+async function str(data) {
+  return data.toString();
+}
+async function int(data) {
+  return parseInt(data);
+}
+async function float(data) {
+  return parseFloat(data);
+}
+async function bool(data) {
+  return data == true;
 }
